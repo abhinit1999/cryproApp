12e5ce83f1fa8c365ed97691cbfcf03580fedcd0
Abhinit <krabhinit60@gmail.com>
2022-05-17 18:37:32 +0530

    nothing to commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Crypto Tracker App Started developing from today</h1>
+     <h1>Crypto Tracker App Started developing from today
+       and the best part is that I am learning React.js
+     </h1>
     </div>
   );
 }
